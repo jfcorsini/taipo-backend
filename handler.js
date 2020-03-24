364120341ed59dd855ad5b6e04b3c35916899a46
João Corsini <jf.corsini@gmail.com>
2020-03-24 17:15:40 +0200

    refactor: Move createPrivateChat to own file

--- handler.js
+++ handler.js
@@ -1,8 +1,4 @@
-const AWS = require('aws-sdk');
-const uuid = require('uuid');
-const { isEmpty } = require('ramda');
-
-const documentClient = new AWS.DynamoDB.DocumentClient();
+const createPrivateChat = require('./src/createPrivateChat');
 
 exports.app = async (event, context) => {
   console.log("Received event {}", JSON.stringify(event, 3));
@@ -10,67 +6,7 @@ exports.app = async (event, context) => {
   console.log("Got an Invoke Request.");
   switch(event.field) {
       case "createPrivateChat":
-          const username = event.arguments.input.username;
-          const identityUsername = event.identityUsername;
-          const createdAt = event.createdAt;
-
-          const sortedUsernames = [username, identityUsername].sort().join('_');
-          const chatId = uuid.v4();
-
-          const privateChat = {
-            chatId,
-            sortKey: `config_${sortedUsernames}`,
-            createdAt,
-            private: true,
-          };
-
-          const params = {
-            RequestItems: {
-              [process.env.CHATS_TABLE]: [
-                {
-                  PutRequest: {
-                    Item: privateChat,
-                  }
-                },
-                {
-                  PutRequest: {
-                    Item: {
-                      chatId,
-                      sortKey: `member_${identityUsername}`,
-                      createdAt,
-                      username: identityUsername,
-                    }
-                  }
-                },
-                {
-                  PutRequest: {
-                    Item: {
-                      chatId,
-                      sortKey: `member_${username}`,
-                      createdAt,
-                      username,
-                    }
-                  }
-                }
-              ]
-            }
-          };
-
-          try {
-            const response = await documentClient.batchWrite(params).promise();
-            
-            if (isEmpty(response.UnprocessedItems)) {
-              return privateChat;
-            }
-
-            console.log('Unprocessed items', response.UnprocessedItems);
-            throw new Error('Failed to create chat')
-            
-          } catch (error) {
-            console.error(error, 'Failed to run batchWrite');
-            throw error;
-          }
-
+          return createPrivateChat(event, context);
       default:
           throw new Error("Unknown field, unable to resolve" + event.field);
   }
---
+++ src/createPrivateChat.js
@@ -0,0 +1,69 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const { isEmpty } = require('ramda');
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const handler = (event, context) => {
+  const { createdAt, identityUsername, arguments: { input } } = event;
+  const { username } = input;
+
+  const sortedUsernames = [username, identityUsername].sort().join('_');
+  const chatId = uuid.v4();
+
+  const privateChat = {
+    chatId,
+    sortKey: `config_${sortedUsernames}`,
+    createdAt,
+    private: true,
+  };
+
+  const params = {
+    RequestItems: {
+      [process.env.CHATS_TABLE]: [
+        {
+          PutRequest: {
+            Item: privateChat,
+          }
+        },
+        {
+          PutRequest: {
+            Item: {
+              chatId,
+              sortKey: `member_${identityUsername}`,
+              createdAt,
+              username: identityUsername,
+            }
+          }
+        },
+        {
+          PutRequest: {
+            Item: {
+              chatId,
+              sortKey: `member_${username}`,
+              createdAt,
+              username,
+            }
+          }
+        }
+      ]
+    }
+  };
+
+  try {
+    const response = await documentClient.batchWrite(params).promise();
+    
+    if (isEmpty(response.UnprocessedItems)) {
+      return privateChat;
+    }
+
+    console.log('Unprocessed items', response.UnprocessedItems);
+    throw new Error('Failed to create chat')
+    
+  } catch (error) {
+    console.error(error, 'Failed to run batchWrite');
+    throw error;
+  }
+};
+
+module.exports = handler;
