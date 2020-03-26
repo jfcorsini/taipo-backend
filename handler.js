9b0d98cff32e51250bdc59e320e6ea5426a347c5
João Corsini <jf.corsini@gmail.com>
2020-03-26 16:08:27 +0200

    Revert "feat: Move GetChat to Lambda source"
    This reverts commit b66e080a2ae8758c140c9e3cd29a754b52a2fa2a.

--- handler.js
+++ handler.js
@@ -1,19 +1,16 @@
 const createPrivateChat = require('./src/createPrivateChat');
 const getPrivateChat = require('./src/getPrivateChat');
-const getChat = require('./src/getChat');
 
 exports.app = (event, context) => {
   console.log("Received event {}", JSON.stringify(event, 3));
 
   console.log("Got an Invoke Request.");
   switch(event.field) {
-    case "createPrivateChat":
-      return createPrivateChat(event, context);
-    case "getPrivateChat":
-      return getPrivateChat(event, context);
-    case "getChat":
-      return getChat(event, context);
-    default:
-      throw new Error("Unknown field, unable to resolve" + event.field);
+      case "createPrivateChat":
+          return createPrivateChat(event, context);
+      case "getPrivateChat":
+          return getPrivateChat(event, context);
+      default:
+          throw new Error("Unknown field, unable to resolve" + event.field);
   }
 };
