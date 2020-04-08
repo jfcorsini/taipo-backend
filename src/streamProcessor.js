3573a76ce53fc1f6783409c2eba0d29499403d30
João Corsini <jf.corsini@gmail.com>
2020-04-08 13:39:11 +0300

    fix: Handle messages to translate with bad data
    If message is missing or empty, it should not be translated.

--- src/streamProcessor.js
+++ src/streamProcessor.js
@@ -4,20 +4,21 @@ const updateTranslation = require('./lib/updateTranslation');
 module.exports = async (event, context) => {
   console.log("Received event {}", JSON.stringify(event, 3));
 
-  const messagesToTranslate = event.Records
+  const newMessages = event.Records
     .filter((record) => {
       const sortKey = pathOr('', ['dynamodb', 'NewImage', 'sortKey', 'S'], record);
 
       return record.eventName === 'INSERT' && startsWith('message_', sortKey);
-    })
-    .map((record) => {
-      const entry = record.dynamodb.NewImage;
-      return {
-        chatId: entry.chatId.S,
-        sortKey: entry.sortKey.S,
-        message: entry.message.S,
-      };
     });
+
+  const messagesToTranslate = newMessages.map((record) => {
+    const entry = record.dynamodb.NewImage;
+    return {
+      chatId: entry.chatId.S,
+      sortKey: entry.sortKey.S,
+      message: pathOr(null, ['message', 'S'], entry),
+    };
+  }).filter(item => item.message);
   
   await updateTranslation(messagesToTranslate);
 };
