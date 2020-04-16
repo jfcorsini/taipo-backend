eafb83467f4e171d326fe9ef2d5f93de18ba8905
João Corsini <jf.corsini@gmail.com>
2020-04-16 18:05:23 +0300

    feat: Pass room name when requesting token

--- src/lib/getTwilioToken.js
+++ src/lib/getTwilioToken.js
@@ -4,22 +4,22 @@ const VideoGrant = AccessToken.VideoGrant;
 const MAX_ALLOWED_SESSION_DURATION = 14400;
 
 const handler = async (event, context) => {
-  const { identityUsername } = event;
+  const { identityUsername, arguments: { input } } = event;
+  const { roomName } = input;
 
-  const token = new AccessToken(
+  const accessToken = new AccessToken(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_API_SID,
     process.env.TWILIO_API_SECRET,
-    { ttl: MAX_ALLOWED_SESSION_DURATION }
+    { ttl: MAX_ALLOWED_SESSION_DURATION, identity: identityUsername }
   );
-  token.identity = identityUsername;
 
-  const grant = new VideoGrant();
-  token.addGrant(grant);
+  const grant = new VideoGrant({ room: roomName});
+  accessToken.addGrant(grant);
 
   const result = {
     username: identityUsername,
-    token: token.toJwt(),
+    token: accessToken.toJwt(),
   };
   console.log(result, 'Resulting token');
 
